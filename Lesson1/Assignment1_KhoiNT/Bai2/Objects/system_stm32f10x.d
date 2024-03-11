cadd552336391aedd8ff83b982f3a53fba385963
khoint373 <162561330+khoint373@users.noreply.github.com>
2024-03-11 22:00:15 +0700

    Add files via upload

--- Lesson1/Assignment1_KhoiNT/Bai2/Objects/system_stm32f10x.d
+++ Lesson1/Assignment1_KhoiNT/Bai2/Objects/system_stm32f10x.d
@@ -1,13 +1,13 @@
-./objects/system_stm32f10x.o: RTE\Device\STM32F103C8\system_stm32f10x.c \
-  C:\Keil_v5\ARM\Pack\Keil\STM32F1xx_DFP\2.4.1\Device\Include\stm32f10x.h \
-  RTE\_Target_1\RTE_Components.h \
-  C:\Keil_v5\ARM\Pack\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm3.h \
-  C:\Keil_v5\ARM\ARMCLANG\include\stdint.h \
-  C:\Keil_v5\ARM\Pack\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
-  C:\Keil_v5\ARM\Pack\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
-  C:\Keil_v5\ARM\Pack\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
-  C:\Keil_v5\ARM\Pack\Keil\STM32F1xx_DFP\2.4.1\Device\Include\system_stm32f10x.h \
-  RTE\Device\STM32F103C8\stm32f10x_conf.h \
-  C:\Keil_v5\ARM\Pack\Keil\STM32F1xx_DFP\2.4.1\Device\StdPeriph_Driver\inc\stm32f10x_gpio.h \
-  C:\Keil_v5\ARM\Pack\Keil\STM32F1xx_DFP\2.4.1\Device\StdPeriph_Driver\inc\stm32f10x_rcc.h \
-  C:\Keil_v5\ARM\Pack\Keil\STM32F1xx_DFP\2.4.1\Device\StdPeriph_Driver\inc\misc.h
+.\objects\system_stm32f10x.o: RTE\Device\STM32F103C8\system_stm32f10x.c
+.\objects\system_stm32f10x.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\1.1.0\Device\Include\stm32f10x.h
+.\objects\system_stm32f10x.o: C:\Users\kh513\Desktop\STM32\Lesson1\Assignment1_KhoiNT\Bai2\RTE\RTE_Components.h
+.\objects\system_stm32f10x.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.1.0\CMSIS\Include\core_cm3.h
+.\objects\system_stm32f10x.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\stdint.h
+.\objects\system_stm32f10x.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.1.0\CMSIS\Include\core_cmInstr.h
+.\objects\system_stm32f10x.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.1.0\CMSIS\Include\core_cmFunc.h
+.\objects\system_stm32f10x.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\1.1.0\Device\Include\system_stm32f10x.h
+.\objects\system_stm32f10x.o: C:\Users\kh513\Desktop\STM32\Lesson1\Assignment1_KhoiNT\Bai2\RTE\Device\STM32F103C8\stm32f10x_conf.h
+.\objects\system_stm32f10x.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\1.1.0\Device\StdPeriph_Driver\inc\stm32f10x_gpio.h
+.\objects\system_stm32f10x.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\1.1.0\Device\Include\stm32f10x.h
+.\objects\system_stm32f10x.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\1.1.0\Device\StdPeriph_Driver\inc\stm32f10x_rcc.h
+.\objects\system_stm32f10x.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\1.1.0\Device\StdPeriph_Driver\inc\misc.h
--- Lesson1/Assignment1_KhoiNT/Bai2/main.c
+++ Lesson1/Assignment1_KhoiNT/Bai2/main.c
@@ -1,12 +1,56 @@
+/******************************************************************************
+The program is built to handle the problem of changing LED status. The button
+is configured for pin PC15 and changes the LED status at pin PC13.
+Author: KhoiNT
+******************************************************************************/
+
+/******************************************************************************
+	* Include
+******************************************************************************/
 #include "stm32f10x.h"
 #include "stm32f10x_rcc.h"
 #include "stm32f10x_gpio.h"
 #include "stdint.h"
 
-void RCC_Config(void) {
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+/******************************************************************************
+	* Prototype
+******************************************************************************/
+void RCC_Config(void);
+void GPIO_Config(void);
+void LED_toggle(void);
+
+/******************************************************************************
+	* Code
+******************************************************************************/
+int main() {
+	RCC_Config();
+	GPIO_Config();
+	
+	while(1) {
+	 if(GPIO_ReadInputDataBit(GPIOC,GPIO_Pin_15)) {
+		while(GPIO_ReadInputDataBit(GPIOC,GPIO_Pin_15));
+		LED_toggle();
+	 }
+	}
+
+	return 0;
 }
 
+/**
+  * @brief  Enables or disables peripheral clock.
+  * @param  None.
+  * @return None.
+  */
+void RCC_Config(void) {
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+}
+
+/**
+  * @brief  Initializes the GPIOx peripheral according to the specified
+						parameters in the GPIO_InitStruct.
+  * @param  None.
+  * @return None.
+  */
 void GPIO_Config(void) {
 	GPIO_InitTypeDef GPIO_InitStruct;
 	/* Config pin PC13 as Led's pin */
@@ -14,35 +58,30 @@ void GPIO_Config(void) {
 	GPIO_InitStruct.GPIO_Pin   = GPIO_Pin_13;
 	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
 	
-	GPIO_Init(GPIOA, &GPIO_InitStruct);
+	GPIO_Init(GPIOC, &GPIO_InitStruct);
 	
-	/* Config pin PC5 as Button's pin */
+	/* Config pin PC15 as Button's pin */
 	GPIO_InitStruct.GPIO_Mode  = GPIO_Mode_IPU;
-	GPIO_InitStruct.GPIO_Pin   = GPIO_Pin_5;
+	GPIO_InitStruct.GPIO_Pin   = GPIO_Pin_15;
 	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
 	
-	GPIO_Init(GPIOA, &GPIO_InitStruct);
+	GPIO_Init(GPIOC, &GPIO_InitStruct);
 }
 
+/**
+  * @brief  Toggle the LED.
+  * @param  None.
+  * @return None.
+  */
 //void LED_toggle(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin)
 void LED_toggle(void) {
-	if ( GPIO_ReadOutputDataBit(GPIOA,GPIO_Pin_13) ) {
-		GPIO_ResetBits(GPIOA,GPIO_Pin_13);
+	if ( GPIO_ReadOutputDataBit(GPIOC,GPIO_Pin_13) ) {
+		GPIO_ResetBits(GPIOC,GPIO_Pin_13);
 	} else {
-		GPIO_SetBits(GPIOA,GPIO_Pin_13);
+		GPIO_SetBits(GPIOC,GPIO_Pin_13);
 	}
 }
 
-int main() {
-	RCC_Config();
-	GPIO_Config();
-	
-	while(1) {
-	 if(GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_5)) {
-		while(GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_5));
-		LED_toggle();
-	 }
-	}
-
-	return 0;
-}
+/******************************************************************************
+	* End of file
+******************************************************************************/
